2785eb1ac79f3f6d90592674dab13355ab18f680
charun umesh <charunumesh@gmail.com>
2021-07-26 13:37:10 +0530

    deployment done

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9000'
-})
+  baseURL: "https://messanger-mern.herokuapp.com/",
+  // baseURL: 'http://localhost:9000'
+});
 
-export default instance
+export default instance;
